e28b613694f10bb1a1a48664c024e4bfac34e1c4
codewithamitpatil <62344675+codewithamitpatil@users.noreply.github.com>
2021-08-21 11:41:42 +0530

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,14 +3,14 @@ const express = require('express');
 const router  = express.Router();
 
 // importing  routes
-//const auth_v1 = require('./auth');
+const auth_v1 = require('./auth');
 const user_v1 = require('./users');
 const todos_v1 = require('./todos');
 const carts_v1 = require('./carts');
 const products_v1 = require('./products');
 
 // intialize routes
-//router.use('/auth',auth_v1);
+router.use('/auth',auth_v1);
 router.use('/users',user_v1);
 router.use('/todos',todos_v1);
 router.use('/carts',carts_v1 );
